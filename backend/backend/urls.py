8bacdee16d3465c63884b8b10fe90ce2a83af524
yeshenghao <shenghao@LAPTOP-9D13OQT1>
2025-05-03 16:08:47 +0200

    merge

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -27,5 +27,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('backend.api.urls')), 
+    path('api/', include('api.urls')), 
 ]
