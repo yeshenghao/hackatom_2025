3e9310dfb1359e94adeba95989e47e4c56269490
jiali.yang <jiali.yang@estudiantat.upc.edu>
2025-05-04 04:34:01 +0200

    funciona el menu

---
+++ frontend/vue-project/src/Sobre_nosotros.vue
@@ -0,0 +1,46 @@
+
+
+<template>
+  <div class="home">
+    nosotros
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style scoped>
+.home {
+  max-width: 600px;
+  margin: 2rem auto;
+  padding: 1.5rem;
+  text-align: center;
+  font-family: Arial, sans-serif;
+  background-color: #f9f9f9;
+  border-radius: 12px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+  color: #007bff;
+}
+
+p {
+  margin-bottom: 1rem;
+  font-size: 1.1rem;
+  color: #333;
+}
+
+button {
+  padding: 0.75rem 1.5rem;
+  font-size: 1rem;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+}
+</style>
--- frontend/vue-project/src/components/Navbar.vue
+++ frontend/vue-project/src/components/Navbar.vue
@@ -1,9 +1,25 @@
+<script lang="ts" setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const handleSelect = (path: string) => {
+  router.push(path) // Navega a la ruta seleccionada
+}
+</script>
+
 <template>
-	<el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" background-color="#545c64"
-		text-color="#fff" active-text-color="#ffd04b" @select="handleSelect">
-		<el-menu-item index="1">Inicio</el-menu-item>
+	<el-menu :default-active="$route.path"
+		class="el-menu-demo" 
+		mode="horizontal" 
+		background-color="#545c64"
+		text-color="#fff" 
+		active-text-color="#ffd04b" 
+		@select="handleSelect"
+		>
+		<el-menu-item index="/">Home</el-menu-item>
 		<el-sub-menu index="2">
-			<template #title>Modelos</template>
+			<template #title>Models</template>
 			<el-menu-item index="2-1">Un modelo</el-menu-item>
 			<el-menu-item index="2-2">Otro modelo</el-menu-item>
 			<el-menu-item index="2-3">Modelo 3</el-menu-item>
@@ -14,16 +30,7 @@
 				<el-menu-item index="2-4-3">Serie s three</el-menu-item>
 			</el-sub-menu>
 		</el-sub-menu>
-		<el-menu-item index="3" disabled>Questionario</el-menu-item>
-		<el-menu-item index="4">Reto diario</el-menu-item>
+		<el-menu-item index="/test">Test</el-menu-item>
+		<el-menu-item index="/sobre-nosotros">About Us</el-menu-item>
 	</el-menu>
 </template>
-
-<script lang="ts" setup>
-import { ref } from 'vue'
-
-const activeIndex = ref('1')
-const handleSelect = (key: string, keyPath: string[]) => {
-	console.log(key, keyPath)
-}
-</script>
